72c9e2be7db42e16fc7340933984baeacf50a133
Hashini <hashiniridmika238@gmail.com>
2023-03-24 01:41:36 +0530

    Install Routing packadge and create some routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Product from "./pages/Product";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="/Product" element={<Product />}></Route>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
