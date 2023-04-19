c77633fee8b0c747f2663fbad4313b8e8c6619cd
Hashini <hashiniridmika238@gmail.com>
2023-04-19 13:19:17 +0530

    New logo updated

--- src/components/Login/Logo.js
+++ src/components/Login/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LOGIN from "../../assets/images/solarlogo.png";
+import LOGIN from "../../assets/images/solar_life_logo.png";
 
 export default function Logo() {
   return (
