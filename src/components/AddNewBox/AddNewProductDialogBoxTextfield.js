b62e7bbd3df8f7fc0b794e803e5c0eb7b55d4a3f
Hashini <hashiniridmika238@gmail.com>
2023-04-29 12:52:14 +0530

    Add new dialog box component is created.

---
+++ src/components/AddNewBox/AddNewProductDialogBoxTextfield.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+export default function AddNewProductDialogBoxTextfield() {
+  return (
+    <div>
+      <TextField variant="outlined" />
+    </div>
+  );
+}
