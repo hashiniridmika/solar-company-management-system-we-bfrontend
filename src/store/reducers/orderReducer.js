00fc5f48c92ae57ce259eadbc589bc28baac0a19
Hashini <hashiniridmika238@gmail.com>
2023-05-03 02:08:35 +0530

    Order reducer view part is done

---
+++ src/store/reducers/orderReducer.js
@@ -0,0 +1,33 @@
+import * as Actions from "../actions/orderAction";
+
+const inisialState = {
+  getAllOrderListLoading: "notStarted",
+  allOrderList: [],
+};
+
+const orderReducer = (state = inisialState, action) => {
+  switch (action.type) {
+    case Actions.GET_ALL_ORDERS_LOADING:
+      return { ...state, getAllOrderListLoading: "loading" };
+
+    case Actions.GET_ALL_ORDERS_SUCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        getAllOrderListLoading: "sucess",
+        allOrderList: action.payload,
+      };
+
+    case Actions.GET_ALL_ORDERS_FAIL:
+      return {
+        ...state,
+        getAllOrderListLoading: "fail",
+        allOrderList: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default orderReducer;
