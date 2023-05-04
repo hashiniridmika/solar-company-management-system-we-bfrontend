29db1889bd02ecf344e2234fda02d7ea355e5593
Hashini <hashiniridmika238@gmail.com>
2023-05-04 16:09:20 +0530

    product table delete cancel button is done

---
+++ src/components/AddNewBox/CancelProductButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+export default function CancelProductButton() {
+  return (
+    <div>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#FFFFFF",
+          color: "#00C569", // Set font color to green
+          borderColor: "#00C569", // Set border color to green
+          textTransform: "none",
+          width: 150,
+          "&:hover": {
+            backgroundColor: "#FFFFFF",
+            opacity: 0.8,
+          },
+          "&:active": {
+            backgroundColor: "#FFFFFF",
+            opacity: 0.5,
+          },
+        }}
+      >
+        NO
+      </Button>
+    </div>
+  );
+}
