2218b883af7f3cb730a645efb4d2ee43f2d3b98c
Hashini <hashiniridmika238@gmail.com>
2023-05-04 16:10:56 +0530

    Product edit button is done

---
+++ src/components/AddNewBox/EditProductButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+export default function EditProductButton() {
+  return (
+    <div>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#00C569",
+          textTransform: "none",
+          width: 150,
+          "&:hover": {
+            backgroundColor: "#00C569", // Change hover background color
+            opacity: 0.8,
+          },
+          "&:active": {
+            backgroundColor: "#00C569", // Change active background color
+            opacity: 0.5,
+          },
+        }}
+      >
+        Update
+      </Button>
+    </div>
+  );
+}
