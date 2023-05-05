e5318b1d2990505e7c584c63788ea363a6b3f717
Hashini <hashiniridmika238@gmail.com>
2023-05-05 16:48:02 +0530

    Order details dialog box is created

---
+++ src/layouts/OrderDetails/OrderDetailsDialogBox.js
@@ -0,0 +1,34 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import AddNewDialogBoxTextfield from "../../components/AddNewBox/AddNewDialogBoxTextfield";
+import OrderStatusSelectBox from "../../components/OrderBox/OrderStatusSelectBox";
+import OrderSaveButton from "../../components/OrderBox/OrderSaveButton";
+
+export default function OrderDetailsDialogBox() {
+  return (
+    <div>
+      <Typography align="center" sx={{ pt: 2, fontSize: 30, color: "#00C569" }}>
+        Order Details
+      </Typography>
+      <Typography
+        align="center"
+        sx={{ padding: 1.5, fontSize: 13, color: "#6D6D6D" }}
+      >
+        Welcome to the "Order Details" page. View and manage customer orders,
+        update status and export data.
+      </Typography>
+      <Grid container>
+        <Grid item xs={6}>
+          <AddNewDialogBoxTextfield fieldname="Order Id" />
+          <AddNewDialogBoxTextfield fieldname="Payment Type" />
+          <AddNewDialogBoxTextfield fieldname="Order By" />
+        </Grid>
+        <Grid item xs={6}>
+          <OrderStatusSelectBox fieldintro="Order Status" />
+
+          <OrderSaveButton />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
