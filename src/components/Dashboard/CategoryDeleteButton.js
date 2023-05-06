1bf445af48333966f6a72cba629ed57c3f65caf8
Hashini <hashiniridmika238@gmail.com>
2023-05-06 11:55:18 +0530

    Category Delete button is done

---
+++ src/components/Dashboard/CategoryDeleteButton.js
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function CategoryDeleteButton() {
+  return (
+    <div>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#00C569",
+          textTransform: "none",
+          width: 150,
+
+          "&:hover": {
+            backgroundColor: "#00C569", // Change hover background color
+            opacity: 0.8,
+          },
+          "&:active": {
+            backgroundColor: "#00C569", // Change active background color
+            opacity: 0.5,
+          },
+        }}
+      >
+        Delete
+      </Button>
+    </div>
+  );
+}
