8f17ed9d92032ee702b59ffcd0d8e114c96250c2
Hashini <hashiniridmika238@gmail.com>
2023-05-06 21:45:07 +0530

    Dashboard category items are created

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SideNav from "../layouts/common/SideNav";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import DashboardDetails from "../layouts/Dashboard/DashboardDetails";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCatergories } from "../store/actions/catergoryAction";
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+
+  const { allCategoryList } = useSelector((store) => store.catergoryReducer);
+
+  useEffect(() => {
+    if (allCategoryList.length === 0) {
+      dispatch(getAllCatergories());
+    }
+  }, [allCategoryList.length, dispatch]);
+
   return (
     <div>
       <Grid2 container>
