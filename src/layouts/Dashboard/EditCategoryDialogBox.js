0f1719433a36a0e3e11b9150d916d285c732b4c4
Hashini <hashiniridmika238@gmail.com>
2023-05-06 11:57:46 +0530

    Edit category dialog box done

---
+++ src/layouts/Dashboard/EditCategoryDialogBox.js
@@ -0,0 +1,31 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import AddNewDialogBoxTextfield from "../../components/AddNewBox/AddNewDialogBoxTextfield";
+import CategoryUpdateButton from "../../components/Dashboard/CategoryUpdateButton";
+import CategoryDeleteButton from "../../components/Dashboard/CategoryDeleteButton";
+
+export default function EditCategoryDialogBox() {
+  return (
+    <div>
+      <Typography align="center" sx={{ pt: 2, fontSize: 25, color: "#00C569" }}>
+        Edit Category
+      </Typography>
+      <Grid container>
+        <Grid item xs={12}>
+          <AddNewDialogBoxTextfield
+            fieldname="Category"
+            placeholder="Category Name"
+          />
+        </Grid>
+        <Grid container sx={{ mt: 4, paddingLeft: 6 }}>
+          <Grid item xs={6}>
+            <CategoryUpdateButton />
+          </Grid>
+          <Grid item xs={6}>
+            <CategoryDeleteButton />
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
