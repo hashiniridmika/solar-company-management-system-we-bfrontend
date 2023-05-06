1e563425a737e121d6f2e372488521c65a9878fc
Hashini <hashiniridmika238@gmail.com>
2023-05-06 21:47:04 +0530

    dashboard add category button is created

--- src/layouts/Dashboard/AddCategoryDialogBox.js
+++ src/layouts/Dashboard/AddCategoryDialogBox.js
@@ -1,5 +1,24 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import AddNewDialogBoxTextfield from "../../components/AddNewBox/AddNewDialogBoxTextfield";
+import CategorySaveButton from "../../components/Dashboard/CategorySaveButton";
 
 export default function AddCategoryDialogBox() {
-  return <div>AddCategoryDialogBox****</div>;
+  return (
+    <div>
+      <Typography align="center" sx={{ pt: 2, fontSize: 25, color: "#00C569" }}>
+        Add New Category
+      </Typography>
+
+      <div>
+        <AddNewDialogBoxTextfield
+          fieldname="New Category"
+          placeholder="Enter Category Name"
+        />
+      </div>
+      <Box display="flex" justifyContent="center" mt={4}>
+        <CategorySaveButton />
+      </Box>
+    </div>
+  );
 }
