5701a815c94241c38617714796acd9acc9b7cb1d
Hashini <hashiniridmika238@gmail.com>
2023-05-14 00:35:59 +0530

    Stock edit action is done'

--- src/store/actions/stockAction.js
+++ src/store/actions/stockAction.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-
+//View
 export const GET_ALL_STOCKS_LOADING = "GET_ALL_STOCKS_LOADING";
 export const GET_ALL_STOCKS_SUCESS = "GET_ALL_STOCKS_SUCESS";
 export const GET_ALL_STOCKS_FAIL = "GET_ALL_STOCKS_FAIL";
+
+export const SET_USER_SELECTED_STOCK = "SET_USER_SELECTED_STOCK";
 
 export const getAllStocks = () => {
   return (dispatch) => {
@@ -20,5 +22,11 @@ export const getAllStocks = () => {
         console.log(e);
         dispatch({ type: GET_ALL_STOCKS_FAIL });
       });
+  };
+};
+
+export const setUserSelectedStock = (stock) => {
+  return (dispatch) => {
+    dispatch({ type: SET_USER_SELECTED_STOCK, payload: stock });
   };
 };
