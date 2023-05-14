66834038c7ff435d532210b03feb29060756c6bb
Hashini <hashiniridmika238@gmail.com>
2023-05-14 18:49:33 +0530

    Add dashboard image

--- src/components/Dashboard/CategoryDeleteButton.js
+++ src/components/Dashboard/CategoryDeleteButton.js
@@ -7,16 +7,17 @@ export default function CategoryDeleteButton() {
       <Button
         variant="contained"
         sx={{
-          backgroundColor: "#00C569",
+          backgroundColor: "#FFFFFF",
+          color: "#00C569", // Set font color to green
+          borderColor: "#00C569", // Set border color to green
           textTransform: "none",
           width: 150,
-
           "&:hover": {
-            backgroundColor: "#00C569", // Change hover background color
+            backgroundColor: "#FFFFFF",
             opacity: 0.8,
           },
           "&:active": {
-            backgroundColor: "#00C569", // Change active background color
+            backgroundColor: "#FFFFFF",
             opacity: 0.5,
           },
         }}
--- src/components/Dashboard/DashboardAnaliticImage.js
+++ src/components/Dashboard/DashboardAnaliticImage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ANALITIC from "../../assets/images/analitic.png";
+import SENTI from "../../assets/images/sentiBar.png";
 import { Typography } from "@mui/material";
 
 export default function DashboardAnaliticImage() {
@@ -25,9 +26,24 @@ export default function DashboardAnaliticImage() {
         <Typography sx={{ color: "#7B7B7B", pt: 2, pl: 10 }}>
           COMMUNITY SENTIMENT
         </Typography>
+        <Typography
+          sx={{
+            color: "#000000",
+            pt: 1,
+            pl: 10,
+            fontWeight: "bold",
+            fontSize: 22,
+          }}
+        >
+          Overwhelmingly positive
+        </Typography>
       </div>
+      <div style={{ position: "absolute", bottom: "4%", left: "9%" }}>
+        <img src={SENTI} alt="SenimantBarImage" style={{ width: "80%" }}></img>
+      </div>
+
       <div style={{ position: "absolute", bottom: "3%", right: "10%" }}>
-        <img src={ANALITIC} alt="AnaliticImage" style={{ width: "70%" }} />
+        <img src={ANALITIC} alt="AnaliticImage" style={{ width: "80%" }} />
       </div>
     </div>
   );
