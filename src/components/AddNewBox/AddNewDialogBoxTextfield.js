e6e9a070efba7950a00e57b9be19458f07104663
Hashini <hashiniridmika238@gmail.com>
2023-05-14 15:09:35 +0530

    Past the value for textfield

--- src/components/AddNewBox/AddNewDialogBoxTextfield.js
+++ src/components/AddNewBox/AddNewDialogBoxTextfield.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { TextField, Typography } from "@mui/material";
 
-export default function AddNewDialogBoxTextfield({ name, placeholder, rows }) {
+export default function AddNewDialogBoxTextfield({
+  fieldname,
+  name,
+  placeholder,
+  rows,
+  value,
+  handleChange,
+}) {
   return (
     <div
       style={{ paddingLeft: "20px", paddingTop: "20px", paddingRight: "20px" }}
     >
       <Typography color="#000000" fontSize={14}>
-        {name}
+        {fieldname}
       </Typography>
       <TextField
+        value={value}
+        onChange={(e) => handleChange(e.target.value, name)}
         sx={{ borderRadius: 10 }}
         placeholder={placeholder}
         size="small"
