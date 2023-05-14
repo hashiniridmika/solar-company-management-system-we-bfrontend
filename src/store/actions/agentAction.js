09594948436c3148869ee7ab8d035a3300ebfa6b
Hashini <hashiniridmika238@gmail.com>
2023-05-14 23:21:04 +0530

    Edit button agent action is done

--- src/store/actions/agentAction.js
+++ src/store/actions/agentAction.js
@@ -43,7 +43,7 @@ export const updateAgent = (user) => {
       .post("http://localhost:5002/agent/update", { user })
       .then((response) => {
         console.log(response.data);
-        dispatch({ type: UPDATE_AGENTS_SUCESS, payload: response.data.agent });
+        dispatch({ type: UPDATE_AGENTS_SUCESS, payload: response.data.user });
       })
       .catch((e) => {
         console.log(e);
