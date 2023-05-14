75149ef3ee2bd6b5d5de171dc587ae8c73664f19
Hashini <hashiniridmika238@gmail.com>
2023-05-14 15:19:25 +0530

    root reducer updated

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import agentReducer from "./agentReducer";
 import productItemReducer from "./productitemReducer";
 import stockReducer from "./stockReducer";
 import orderReducer from "./orderReducer";
+import catergoryReducer from "./catergoryReducer";
 
 const rootReducer = combineReducers({
   agentReducer,
   productItemReducer,
   stockReducer,
   orderReducer,
+  catergoryReducer,
 });
 
 export default rootReducer;
