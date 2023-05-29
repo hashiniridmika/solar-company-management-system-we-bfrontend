20f18f29ee41be7b266725a5823eaa3a2efcb0b7
Hashini <hashiniridmika238@gmail.com>
2023-05-29 20:20:09 +0530

    Order item with quantity reducer is done

---
+++ src/store/reducers/orderitemWithQuantityReducer.js
@@ -0,0 +1,33 @@
+import * as Actions from "../actions/orderitemWithQuantityAction";
+
+const inisialState = {
+  getAllOrderitemWithQuantityListLoading: "notStarted",
+  allOrderitemWithQuantityList: [],
+};
+
+const orderitemWithQuantityReducer = (state = inisialState, action) => {
+  switch (action.type) {
+    case Actions.GET_ALL_ORDERITEMWITHQUANTITY_LOADING:
+      return { ...state, getAllOrderitemWithQuantityListLoading: "loading" };
+
+    case Actions.GET_ALL_ORDERITEMWITHQUANTITY_SUCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        getAllOrderitemWithQuantityListLoading: "sucess",
+        allOrderitemWithQuantityList: action.payload,
+      };
+
+    case Actions.GET_ALL_ORDERITEMWITHQUANTITY_FAIL:
+      return {
+        ...state,
+        getAllOrderitemWithQuantityListLoading: "fail",
+        allOrderitemWithQuantityList: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default orderitemWithQuantityReducer;
